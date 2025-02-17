fa0e7836baf359bbea43758503c70ff9222fea43
rebehayan <rebehayan@gmail.com>
2025-02-17 15:11:05 +0900

    base 수정

--- src/main.js
+++ src/main.js
@@ -6,4 +6,4 @@ import { setBaseUrl } from "./setBaseUrl.js";
 Initial();
 Input();
 CopyCode();
-setBaseUrl("rebehayan.github.io", "/snippet/");
+setBaseUrl("rebehayan.github.io", "/snippet/public/");
