72dbe7f00a90803b86ae453da37bf642e143197f
rebehayan <rebehayan@gmail.com>
2025-03-23 12:19:55 +0900

    업데이트

--- src/ClipBoard.js
+++ src/ClipBoard.js
@@ -1,5 +1,10 @@
 export const ClipBoard = async (selector) => {
   try {
+    const element = typeof selector === "string" ? document.querySelector(selector) : selector;
+    if (!element) {
+      throw new Error("요소를 찾을 수 없습니다.");
+    }
+
     await navigator.clipboard.writeText(selector.innerText);
   } catch (error) {
     console.log(error);
